fcaf387dc8282e43951e40fee90fb957803b7f9e
degalian <desarrollos.galicia@gmail.com>
2024-02-25 22:04:00 +0100

    commit local 2

---
+++ src/banco/Antproject1.java
@@ -0,0 +1,36 @@
+package banco;
+import java.util.Scanner;
+
+public class Antproject1 {
+
+    public static void main(String[] args) { 
+        operativa(0, null);
+    }
+
+    public static void operativa(float imp, Object par1) {
+   
+        CCuenta banco1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        double saldoActual = banco1.getSaldo();
+        System.out.println("El saldo inicial es de "+ saldoActual +" euros");
+        
+        try {
+            Scanner importe1 = new Scanner (System.in);
+            System.out.println("Cuanto quiere retirar: ");  
+            Double sacar = importe1.nextDouble();
+            banco1.retiro(sacar);
+            System.out.println("Su saldo después de retirar es: "+ banco1.getSaldo());
+        } catch (Exception e) {
+            System.out.print("Fallo al retirar");
+        }
+        try {
+            Scanner importe2 = new Scanner (System.in);
+            System.out.println("Cuanto quiere ingresar: ");  
+            Double meter = importe2.nextDouble();
+            banco1.ingresar(meter);
+            System.out.println("ingresó "+meter+" y ahora su saldo es: "+banco1.getSaldo());
+        } catch (Exception e) {
+            System.out.print("Fallo al ingresar");
+        }
+    }
+    
+}
---
+++ src/banco/CCuenta.java
@@ -0,0 +1,60 @@
+package banco;
+public class CCuenta {   
+    private String nombre;
+    private String cuenta;
+    private double saldo;
+    private double tipoInteres;
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getCuenta() {
+        return cuenta;
+    }
+    public void setCuenta(String cuenta) {
+        this.cuenta = cuenta;
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    public double getTipoInteres() {
+        return tipoInteres;
+    }
+    public void setTipoInteres(double tipoInteres) {
+        this.tipoInteres = tipoInteres;
+    }
+
+    public CCuenta()
+    {
+    }
+    public CCuenta(String nombre, String cuenta, double saldo, double tipoInteres)
+    {
+        this.nombre =nombre;
+        this.cuenta=cuenta;
+        this.saldo=saldo;
+        this.tipoInteres=tipoInteres;
+    }
+
+
+    public void ingresar(double cantidad) throws Exception
+    {
+        if (cantidad<0)
+            throw new Exception("No se puede ingresar una cantidad negativa");
+        setSaldo(getSaldo() + cantidad);
+    }
+
+    public void retiro(double cantidad) throws Exception
+    {
+        if (cantidad <= 0)
+            throw new Exception ("No se puede retirar una cantidad negativa");
+        if (getSaldo()< cantidad)
+            throw new Exception ("No se hay suficiente saldo");
+        setSaldo(getSaldo() - cantidad);
+    }
+}
